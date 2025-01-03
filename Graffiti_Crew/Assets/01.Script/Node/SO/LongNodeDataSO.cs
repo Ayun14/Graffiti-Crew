4a663df5bdcc764eba0d5024bfaeb334bf70fa9b
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-01-03 19:55:46 +0900

    Add | SingleNode, HitNode Base And LongNode 구현

---
+++ Graffiti_Crew/Assets/01.Script/Node/SO/LongNodeDataSO.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public enum LongNodeType
+{
+    // 직선, 곡선
+    Stright, Curve
+}
+
+[CreateAssetMenu(fileName = "LongNodeDataSO", menuName = "SO/Lode/LongNodeDataSO")]
+public class LongNodeDataSO : NodeDataSO
+{
+    [Header("Data")]
+    public LongNodeType longNodeType;
+    public Material lineRendererMat;
+    public List<Vector3> pointList;
+
+    [Header("Curve")]
+    [Range(2, 100)] public int points; // 점의 개수
+    [Range(-30, 30)] public float amplitude; // 진폭 (y축 높이)
+}
