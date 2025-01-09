49b157a85dfbfe60caff6b90c719059dd3d60700
gongsy1221 <gongsy0724@naver.com>
2025-01-09 20:30:01 +0900

    Add |  Dialogue Base

---
+++ Graffiti_Crew/Assets/01.Script/Dialogue/DialogueController.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    //public List<DialogueData> dialogueEntries = new List<DialogueData>();
+
+    //void Start()
+    //{
+    //    LoadDialogueData();
+    //}
+
+    //void LoadDialogueData()
+    //{
+    //    var data = GoogleSheetsToUnity.;
+    //    if (data != null)
+    //    {
+    //        dialogueEntries = data;
+    //        Debug.Log("Dialogue data loaded successfully!");
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("Failed to load dialogue data.");
+    //    }
+    //}
+}
