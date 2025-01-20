fb8dd813f6deeab78ca5d6b13ca8df2af1ea6c2a
reum <ahreun0408@gmail.com>
2025-01-20 14:32:33 +0900

    add | 스테이지 선택 화면 구현

---
+++ Graffiti_Crew/Assets/01.Script/UI/Views/Computer/StoreView.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AH.UI.Views {
+    public class StoreView : UIView {
+        public StoreView(VisualElement topContainer, ViewModel viewModel) : base(topContainer, viewModel) {
+        }
+    }
+}
