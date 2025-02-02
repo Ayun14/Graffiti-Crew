c96340126683d4eae9f08675201d5537ca10a148
reum <ahreun0408@gmail.com>
2025-02-02 22:37:28 +0900

    add | SliderValueSO에 min, max추가

--- Graffiti_Crew/Assets/01.Script/UI/DatabindingSO/SliderValueSO.cs
+++ Graffiti_Crew/Assets/01.Script/UI/DatabindingSO/SliderValueSO.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "SliderValueSO", menuName = "SO/UI/SliderValueSO")]
 public class SliderValueSO : ScriptableObject
 {
+    public float min = 0;
+    public float max = 100;
     public float value;
 }
