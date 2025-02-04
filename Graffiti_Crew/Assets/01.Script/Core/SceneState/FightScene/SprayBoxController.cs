5d151e79f16f05c37422c41a4ebc0e8de84383eb
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-02-04 19:14:16 +0900

    Add | 스프레이 갈때 UI 표시 구현

--- Graffiti_Crew/Assets/01.Script/Core/SceneState/FightScene/SprayBoxController.cs
+++ Graffiti_Crew/Assets/01.Script/Core/SceneState/FightScene/SprayBoxController.cs
@@ -1,13 +1,26 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SprayBoxController : Observer<GameStateController>
 {
     [SerializeField] private float _changeTime = 2f;
     private float _currentTime = 0;
+    private bool _isChangingSpray = false;
+
+    // Slider
+    private Slider _sprayChangeTimeSlider;
 
     private void Awake()
     {
         Attach();
+
+        _sprayChangeTimeSlider = transform.Find("Canvas")
+            .transform.Find("Slider_SprayChangeTime").GetComponent<Slider>();
+        _sprayChangeTimeSlider.value = 0;
+        Color color = _sprayChangeTimeSlider.image.color;
+        color.a = 0;
+        _sprayChangeTimeSlider.image.color = color;
 
         _currentTime = 0;
     }
@@ -31,19 +44,39 @@ public class SprayBoxController : Observer<GameStateController>
             if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
             {
                 GameObject hitObject = hit.collider.gameObject.transform.parent.gameObject;
-                if (hitObject == gameObject)
+                if (hitObject == gameObject && Input.GetMouseButton(0))
                 {
+                    if (!_isChangingSpray) // 처음 감지되었을 때 한 번만 실행
+                    {
+                        _sprayChangeTimeSlider.image.DOFade(1, 0.3f);
+                        _isChangingSpray = true;
+                    }
+
                     _currentTime += Time.deltaTime;
+                    _sprayChangeTimeSlider.value = _currentTime / _changeTime;
+                    _sprayChangeTimeSlider.gameObject.SetActive(true);
+
                     if (_currentTime >= _changeTime)
                     {
                         mySubject.SetIsSprayEmpty(false);
                         mySubject.InvokeSprayChangeEvent();
+                        ResetSprayChange();
                     }
                 }
                 else
-                    _currentTime = 0;
+                {
+                    ResetSprayChange();
+                }
             }
         }
+    }
+
+    private void ResetSprayChange()
+    {
+        _sprayChangeTimeSlider.image.DOFade(0, 0.3f);
+        _sprayChangeTimeSlider.value = 0;
+        _currentTime = 0;
+        _isChangingSpray = false;
     }
 
     public override void NotifyHandle()
