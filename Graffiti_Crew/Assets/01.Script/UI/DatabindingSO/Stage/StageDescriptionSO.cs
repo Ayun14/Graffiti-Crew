ded4d07c097490bf1d80558f3a34f59b604fd7d9
reum <ahreun0408@gmail.com>
2025-02-08 22:47:17 +0900

    add | 스테이지 설명 연결용so

---
+++ Graffiti_Crew/Assets/01.Script/UI/DatabindingSO/Stage/StageDescriptionSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StageDescriptionSO", menuName = "SO/UI/Stage/StageDescriptionSO")]
+public class StageDescriptionSO : ScriptableObject
+{
+    public string title;
+    public string description;
+    public Sprite graffiti;
+}
