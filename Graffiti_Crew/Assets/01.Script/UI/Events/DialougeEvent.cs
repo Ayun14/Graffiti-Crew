d6a8680fcc8dbf969184a13232f64848765d7538
reum <ahreun0408@gmail.com>
2025-02-09 22:45:57 +0900

    add | Dialouge연결

---
+++ Graffiti_Crew/Assets/01.Script/UI/Events/DialougeEvent.cs
@@ -0,0 +1,7 @@
+using System;
+using UnityEngine;
+
+public class DialougeEvent : MonoBehaviour {
+    public static Action ShowDialougeViewEvent;
+    public static Action HideDialougeViewEvent;
+}
---
+++ Graffiti_Crew/Assets/01.Script/UI/Views/DialougeView.cs
@@ -0,0 +1,10 @@
+using AH.UI.ViewModels;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AH.UI.Views {
+    public class DialougeView : UIView {
+        public DialougeView(VisualElement topContainer, ViewModel viewModel) : base(topContainer, viewModel) {
+        }
+    }
+}
