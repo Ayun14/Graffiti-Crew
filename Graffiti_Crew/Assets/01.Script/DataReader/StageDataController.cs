0853fd75791caae50a0e5c34af907b388c1705a9
gongsy1221 <gongsy0724@naver.com>
2025-02-09 21:14:20 +0900

    Fix | StageDataController

--- Graffiti_Crew/Assets/01.Script/DataReader/StageDataController.cs
+++ Graffiti_Crew/Assets/01.Script/DataReader/StageDataController.cs
@@ -12,16 +12,17 @@ public class StageDataController : MonoBehaviour
         ComputerEvent.SelectStageEvent += HandleLoadStageData;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         ComputerEvent.SelectStageEvent -= HandleLoadStageData;
     }
 
     private void HandleLoadStageData(string chapter, string stage)
     {
+        Debug.Log(chapter + stage);
         for (int i = 0; i < _stageData.StageList.Count; i++)
         {
-            if(_stageData.StageList[i].title == $"{chapter}-{stage}")
+            if(_stageData.StageList[i].id == chapter+stage)
             {
                 _stageDescriptionSO.title = _stageData.StageList[i].title;
                 _stageDescriptionSO.description = _stageData.StageList[i].description;
