002d96ed59d6095178a6b563cc66d1692b6f1581
reum <ahreun0408@gmail.com>
2025-02-09 22:36:40 +0900

    add | 폰트 적용, DialogueSO 제작

---
+++ Graffiti_Crew/Assets/01.Script/UI/DatabindingSO/Dialogue/DialogueSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueSO", menuName = "SO/UI/Dialouge/DialogueSO")]
+public class DialogueSO : ScriptableObject
+{
+    public string characterName;
+    public string dialogue;
+    public Sprite profil;
+}
