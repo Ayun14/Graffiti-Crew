941ad19f10d5149384f23ae9b1dc721a562e5693
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-03-05 21:46:11 +0900

    Change | using 제거

--- Graffiti_Crew/Assets/01.Script/01.Managers/ItemSystem.cs
+++ Graffiti_Crew/Assets/01.Script/01.Managers/ItemSystem.cs
@@ -1,12 +1,11 @@
 using AH.SaveSystem;
 using AH.UI.Data;
-using System;
 using System.Collections.Generic;
-using UniRx;
 using UnityEditor;
 using UnityEngine;
 
-public class ItemSystem : MonoBehaviour {
+public class ItemSystem : MonoBehaviour
+{
     private static ItemSystem instance;
 
     public string directoryPath = "Assets/08.SO/SaveDatas/ItemData";
@@ -14,35 +13,45 @@ public class ItemSystem : MonoBehaviour {
 
     private Dictionary<ProductSO, int> _itemDictionary = new Dictionary<ProductSO, int>();
 
-    private void Awake() {
+    private void Awake()
+    {
         instance = this;
         Init();
     }
-    private void Init() {
-        foreach (var data in saveItemList.saveDataSOList) {
+    private void Init()
+    {
+        foreach (var data in saveItemList.saveDataSOList)
+        {
             ProductSO item = Resources.Load($"Products/Sprays/{data.dataName}_ProductSO") as ProductSO;
             var conversionData = data as ItemSaveDataSO;
             instance._itemDictionary.Add(item, conversionData.count);
         }
     }
-    public static void AddItem(ProductSO item) {
-        if (instance._itemDictionary.TryGetValue(item, out int val)) {
+    public static void AddItem(ProductSO item)
+    {
+        if (instance._itemDictionary.TryGetValue(item, out int val))
+        {
             instance._itemDictionary[item] = ++val;
             instance.AddSaveItem(item);
         }
-        else {
+        else
+        {
             instance._itemDictionary.Add(item, 1);
             instance.CreateItemSO(item);
         }
         instance.PrintCurrentItem();
     }
-    public static void RemoveItem(ProductSO item, int count= 1) {
-        if (instance._itemDictionary.TryGetValue(item, out int val)) {
-            if (val - count >= 1) {
+    public static void RemoveItem(ProductSO item, int count = 1)
+    {
+        if (instance._itemDictionary.TryGetValue(item, out int val))
+        {
+            if (val - count >= 1)
+            {
                 instance._itemDictionary[item] = val - count;
                 instance.RemoveSaveItem(item, count);
             }
-            else {
+            else
+            {
                 instance._itemDictionary.Remove(item);
                 instance.RemoveSaveItem(item);
             }
@@ -50,7 +59,8 @@ public class ItemSystem : MonoBehaviour {
         instance.PrintCurrentItem();
     }
 
-    private void CreateItemSO(ProductSO item, int count = 1) {
+    private void CreateItemSO(ProductSO item, int count = 1)
+    {
         ItemSaveDataSO newSO = ScriptableObject.CreateInstance<ItemSaveDataSO>();
         newSO.dataName = item.saveName;
         newSO.ID = newSO.GetInstanceID();
@@ -66,29 +76,40 @@ public class ItemSystem : MonoBehaviour {
         AssetDatabase.SaveAssets();
         AssetDatabase.Refresh();
     }
-    private void AddSaveItem(ProductSO item) {
-        foreach(var data in saveItemList.saveDataSOList) {
+
+    private void AddSaveItem(ProductSO item)
+    {
+        foreach (var data in saveItemList.saveDataSOList)
+        {
             var conversionData = data as ItemSaveDataSO;
-            if (conversionData.itemName == item.itemName) {
+            if (conversionData.itemName == item.itemName)
+            {
                 conversionData.count++;
             }
         }
     }
-    private void RemoveSaveItem(ProductSO item, int count = 1) {
-        foreach (var data in saveItemList.saveDataSOList) {
+    private void RemoveSaveItem(ProductSO item, int count = 1)
+    {
+        foreach (var data in saveItemList.saveDataSOList)
+        {
             var conversionData = data as ItemSaveDataSO;
-            if (conversionData.itemName == item.itemName) {
+            if (conversionData.itemName == item.itemName)
+            {
                 conversionData.count -= count;
             }
         }
     }
-    private void RemoveSaveItem(ProductSO item) {
-        foreach (var data in saveItemList.saveDataSOList) {
+    private void RemoveSaveItem(ProductSO item)
+    {
+        foreach (var data in saveItemList.saveDataSOList)
+        {
             var conversionData = data as ItemSaveDataSO;
-            if (conversionData.itemName == item.itemName) {
+            if (conversionData.itemName == item.itemName)
+            {
                 saveItemList.saveDataSOList.Remove(conversionData);
                 string path = $"{directoryPath}/{conversionData.name}.asset"; // 삭제할 SO 경로
-                if (AssetDatabase.LoadAssetAtPath<ScriptableObject>(path) != null) {
+                if (AssetDatabase.LoadAssetAtPath<ScriptableObject>(path) != null)
+                {
                     AssetDatabase.DeleteAsset(path);
                     AssetDatabase.SaveAssets();
                     AssetDatabase.Refresh();
@@ -98,25 +119,34 @@ public class ItemSystem : MonoBehaviour {
         }
     }
 
-    private void PrintCurrentItem() {
-        foreach (KeyValuePair<ProductSO, int> entry in _itemDictionary) {
+    private void PrintCurrentItem()
+    {
+        foreach (KeyValuePair<ProductSO, int> entry in _itemDictionary)
+        {
             Debug.Log("Key: " + entry.Key + ", Value: " + entry.Value);
         }
     }
-    public static bool CheckTicket(AdmissionTicket[] tickets) {
-        for (int i = 0; i < tickets.Length; i++) {
-            if (instance._itemDictionary.TryGetValue(tickets[i].ticketType, out int val)) {
-                if(tickets[i].count > val) {
+    public static bool CheckTicket(AdmissionTicket[] tickets)
+    {
+        for (int i = 0; i < tickets.Length; i++)
+        {
+            if (instance._itemDictionary.TryGetValue(tickets[i].ticketType, out int val))
+            {
+                if (tickets[i].count > val)
+                {
                     return false;
                 }
-                else { // 갯수가 모자라거나
+                else
+                { // 갯수가 모자라거나
                 }
             }
-            else { // 종류가 없거나
+            else
+            { // 종류가 없거나
                 return false;
             }
         }
-        for (int i = 0; i < tickets.Length; i++) { // 계산
+        for (int i = 0; i < tickets.Length; i++)
+        { // 계산
             RemoveItem(tickets[i].ticketType, tickets[i].count);
         }
         // 입장 가능
--- Graffiti_Crew/Assets/01.Script/UI/Management/TitleUIManagement.cs
+++ Graffiti_Crew/Assets/01.Script/UI/Management/TitleUIManagement.cs
@@ -2,32 +2,33 @@ using AH.SaveSystem;
 using AH.UI.Events;
 using AH.UI.Models;
 using AH.UI.ViewModels;
-using AH.UI.Views;
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static UnityEditor.Profiling.HierarchyFrameDataView;
 
-namespace AH.UI {
+namespace AH.UI
+{
 
-    public class TitleUIManagement : UIManagement {
+    public class TitleUIManagement : UIManagement
+    {
         private TitleViewModel _viewModel;
 
         private DropdownField _saveSlotField;
         private Button _startBtn;
-        
+
         private string slotPath = "UI/Setting/Slots/";
         private SlotSO[] slots;
 
-        protected override void Init() {
+        protected override void Init()
+        {
             base.Init();
             _viewModel = new TitleViewModel(_model as TitleModel);
             slots = Resources.LoadAll<SlotSO>(slotPath);
 
         }
 
-        protected override void SetupViews() {
+        protected override void SetupViews()
+        {
             base.SetupViews();
             VisualElement root = _uiDocument.rootVisualElement;
             _saveSlotField = root.Q<DropdownField>("saveSlot-dropdownField");
@@ -38,10 +39,12 @@ namespace AH.UI {
 
         }
 
-        private void ClickStartBtn(ClickEvent evt) {
+        private void ClickStartBtn(ClickEvent evt)
+        {
             SceneManager.LoadScene("HangOutScene");
         }
-        private void ChangeSlot(ChangeEvent<string> evt) {
+        private void ChangeSlot(ChangeEvent<string> evt)
+        {
             int index = _saveSlotField.index;
             _viewModel.SetSlotIndex(index);
             SlotSO selectSlot = slots[index];
