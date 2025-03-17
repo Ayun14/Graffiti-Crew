df24e4bfc23893a8cfd17ff544ae29db50216ea7
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-03-17 19:34:53 +0900

    Add | StartTimeline 교체 근데 안보임

--- Graffiti_Crew/Assets/01.Script/Core/SceneState/FightScene/FightSceneTimelineController.cs
+++ Graffiti_Crew/Assets/01.Script/Core/SceneState/FightScene/FightSceneTimelineController.cs
@@ -9,6 +9,7 @@ public class FightSceneTimelineController : Observer<GameStateController>, INeed
     [SerializeField] private CinemachineCamera _rivalGraffitiCam;
 
     [SerializeField] private PlayableDirector _beforeFightTimeline;
+    [SerializeField] private PlayableDirector _countDownTimeline;
     private PlayableDirector _finishTimeline;
     private PlayableDirector _resultTimeline;
     private DialogueUIController _dialogueUIController;
@@ -34,6 +35,9 @@ public class FightSceneTimelineController : Observer<GameStateController>, INeed
             if (mySubject.GameState == GameState.Timeline)
                 _beforeFightTimeline.Play();
 
+            if (mySubject.GameState == GameState.CountDown)
+                _countDownTimeline.Play();
+
             if (mySubject.GameState == GameState.Finish)
                 _finishTimeline.Play();
 
@@ -49,6 +53,7 @@ public class FightSceneTimelineController : Observer<GameStateController>, INeed
     {
         if (mySubject != null)
         {
+            Debug.Log("Change Count Down");
             mySubject.ChangeGameState(GameState.CountDown);
         }
     }
@@ -57,6 +62,7 @@ public class FightSceneTimelineController : Observer<GameStateController>, INeed
     {
         if (mySubject != null)
         {
+            Debug.Log("Change Fight");
             mySubject.ChangeGameState(GameState.Fight);
         }
     }
