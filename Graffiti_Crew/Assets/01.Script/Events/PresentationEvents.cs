02308f2853023290702036a1389a15351c58f759
reum <ahreun0408@gmail.com>
2025-03-20 10:35:51 +0900

    add | ui적용, timeline 수정

--- Graffiti_Crew/Assets/01.Script/Events/PresentationEvents.cs
+++ Graffiti_Crew/Assets/01.Script/Events/PresentationEvents.cs
@@ -2,6 +2,6 @@ using System;
 using UnityEngine;
 
 public static class PresentationEvents {
-    public static Action<bool> FadeInOutEvent;  // true : in | false : out
     public static Action<bool> SetFadeEvent;  // true : in | false : out
+    public static Action<bool> FadeInOutEvent;  // true : in | false : out
 }
