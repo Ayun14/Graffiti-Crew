ce4222c1232f6a0253ba2631d349c6ecd7a4b88b
reum <ahreun0408@gmail.com>
2025-03-24 20:13:44 +0900

    add | dropdown드디어 해결

--- Graffiti_Crew/Assets/01.Script/UI/Management/TitleUIManagement.cs
+++ Graffiti_Crew/Assets/01.Script/UI/Management/TitleUIManagement.cs
@@ -47,32 +47,12 @@ namespace AH.UI {
 
             _fadeView = root.Q<VisualElement>("fade-view");
 
-            _saveSlotField.RegisterCallback<PointerDownEvent>(ClickSlot);
             _saveSlotField.RegisterValueChangedCallback(ChangeSlot);
             _startBtn.RegisterCallback<ClickEvent>(ClickStartBtn);
             _exitBtn.RegisterCallback<ClickEvent>(ClickExitBtn);
             _saveSlotField.index = _viewModel.GetSlotIndex();
 
             StartCoroutine(Routine());
-        }
-
-        private void ClickSlot(PointerDownEvent evt) {
-            _saveSlotField.schedule.Execute(() =>
-            {
-                Debug.Log(_saveSlotField.contentContainer);
-                VisualElement popup = _uiDocument.rootVisualElement.Q<VisualElement>("unity-content-container");
-                Debug.Log(popup);
-                //if (popup != null) {
-                //    Debug.Log("Dropdown 팝업 감지 성공! 스타일 적용");
-                //    popup.style.backgroundColor = Color.white;
-                //    popup.style.borderBottomLeftRadius = 5;
-                //    popup.style.borderBottomRightRadius = 5;
-                //}
-                //else {
-                //    Debug.LogWarning("Dropdown 팝업을 찾을 수 없음, 다시 시도...");
-                //    _saveSlotField.schedule.Execute(() => { /* 다시 체크 */ }).StartingIn(100);
-                //}
-            }).StartingIn(100); // 100ms 후에 체크
         }
 
         private IEnumerator Routine() {
@@ -83,12 +63,10 @@ namespace AH.UI {
                 _startBtn.ToggleInClassList("show-btn");
             }
         }
-
         private void ClickExitBtn(ClickEvent evt) {
             Application.Quit();
             Debug.Log("나가기");
         }
-
         private void ClickStartBtn(ClickEvent evt) {
             GameEvents.SaveGameEvent?.Invoke();
             SceneManager.LoadScene("HangOutScene");
@@ -98,14 +76,6 @@ namespace AH.UI {
             _viewModel.SetSlotIndex(index);
             SlotSO selectSlot = slots[index];
             UIEvents.ChangeSlotEvent?.Invoke(selectSlot);
-        }
-        private void FadeInOut(bool active) {
-            if (active) {
-                _fadeView.RemoveFromClassList("fade-out");
-            }
-            else {
-                _fadeView.AddToClassList("fade-out");
-            }
         }
     }
 }
