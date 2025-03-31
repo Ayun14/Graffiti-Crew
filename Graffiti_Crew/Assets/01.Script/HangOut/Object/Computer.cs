fc786446ff6b532422fcdcff25cdbe0a2e832264
gongsy1221 <gongsy0724@naver.com>
2025-03-31 20:28:34 +0900

    Add | new mat, New Transition Hangout->Computer

--- Graffiti_Crew/Assets/01.Script/HangOut/Object/Computer.cs
+++ Graffiti_Crew/Assets/01.Script/HangOut/Object/Computer.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using AH.SaveSystem;
 using System.Threading.Tasks;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using DG.Tweening;
 
 public class Computer : InteractionObject
 {
+    [SerializeField] private Material _transitionMat;
     private AudioSource _hangoutBGM;
 
     protected override void Awake()
@@ -14,6 +17,8 @@ public class Computer : InteractionObject
 
     private async void Start()
     {
+        _transitionMat.SetFloat("_Lerp", 1);
+
         PresentationEvents.SetFadeEvent?.Invoke(true);
         await Task.Delay(1100);
         PresentationEvents.FadeInOutEvent?.Invoke(true);
@@ -21,10 +26,19 @@ public class Computer : InteractionObject
         _hangoutBGM = GameManager.Instance.SoundSystemCompo.PlaySound(SoundType.HangOut, true);
     }
 
-    public async void ComputerSignal()
+    public void ComputerEvent()
     {
-        PresentationEvents.FadeInOutEvent?.Invoke(false);
-        await Task.Delay(1100);
+        StartCoroutine(ComputerRoutine());
+    }
+
+    private IEnumerator ComputerRoutine()
+    {
+        _transitionMat.DOFloat(0.2f, "_Lerp", 1.5f);
+        yield return new WaitForSeconds(2.5f);
+
+        _transitionMat.DOFloat(0f, "_Lerp", 0.5f);
+        yield return new WaitForSeconds(1f);
+
         _hangoutBGM?.GetComponent<SoundObject>().PushObject(true);
         SaveDataEvents.SaveGameEvent?.Invoke("ComputerScene");
     }
