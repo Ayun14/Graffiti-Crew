45b2d412bc27f3b3d7c4230487aaee404e9972bf
Ayun14 <127943296+Ayun14@users.noreply.github.com>
2025-04-02 10:48:55 +0900

    Fix | PressNode 오류 수정

--- Graffiti_Crew/Assets/01.Script/Fight/Node/Nodes/SO/PressNodeDataSO.cs
+++ Graffiti_Crew/Assets/01.Script/Fight/Node/Nodes/SO/PressNodeDataSO.cs
@@ -6,4 +6,5 @@ public class PressNodeDataSO : NodeDataSO
     public Vector3 pos;
     public Sprite sprite;
     [Range(0.1f, 10)] public float pressTime;
+    [Range(0.1f, 1f)] public float possibleRange;
 }
