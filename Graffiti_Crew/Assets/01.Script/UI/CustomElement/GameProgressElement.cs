593967c3eba5ca680b3c32f937da7a6c0d4a24be
reum <ahreun0408@gmail.com>
2025-06-29 22:14:04 +0900

    add | 2-3노드 작업 + progress에 sprite작업 중

--- Graffiti_Crew/Assets/01.Script/UI/CustomElement/GameProgressElement.cs
+++ Graffiti_Crew/Assets/01.Script/UI/CustomElement/GameProgressElement.cs
@@ -22,8 +22,10 @@ namespace AH.UI.CustomElement {
             }
         }
 
-        private VisualElement progressFill;
-        private VisualElement handle;
+        private VisualElement _progressFill;
+        private VisualElement _handle;
+        private VisualElement _jiaImg;
+        private VisualElement _rivalImg;
 
         private float min = 0;
         private float max = 1;
@@ -33,12 +35,10 @@ namespace AH.UI.CustomElement {
             get => min;
             set { min = value; UpdateProgress(); }
         }
-
         public float Max {
             get => max;
             set { max = value; UpdateProgress(); }
         }
-
         public float Value {
             get => value;
             set {
@@ -54,33 +54,50 @@ namespace AH.UI.CustomElement {
 
             AddToClassList("game-progress-element");
 
-            progressFill = new VisualElement();
-            progressFill.AddToClassList("progress-fill");
-            hierarchy.Add(progressFill);
+            _progressFill = new VisualElement();
+            _progressFill.AddToClassList("progress-fill");
+            hierarchy.Add(_progressFill);
 
-            handle = new VisualElement();
-            handle.AddToClassList("progress-handle");
-            hierarchy.Add(handle);
+            _handle = new VisualElement();
+            _handle.AddToClassList("progress-handle");
+            hierarchy.Add(_handle);
+
+            _jiaImg = new VisualElement();
+            _jiaImg.AddToClassList("progress-img");
+            _handle.Add(_jiaImg);
+
+            _rivalImg = new VisualElement();
+            _rivalImg.AddToClassList("progress-img");
+            _handle.Add(_rivalImg);
         }
-
+        public void SetImage(Sprite jia, Sprite rival = null) {
+            if (jia) {
+                Debug.Log(jia.name);
+                _jiaImg.style.backgroundImage = new StyleBackground(jia);
+            }
+            if (rival) {
+                Debug.Log(rival.name);
+                _rivalImg.style.backgroundImage = new StyleBackground(rival);
+            }
+        }
         private void UpdateProgress() {
             float percent = Mathf.InverseLerp(min, max, value);
 
             // 1. 채운 진행률 설정 (퍼센트 기반)
-            progressFill.style.width = Length.Percent(percent * 100f);
+            _progressFill.style.width = Length.Percent(percent * 100f);
 
             // 2. 실제 픽셀 계산
-            float fillWidth = progressFill.resolvedStyle.width;
-            float handleWidth = handle.resolvedStyle.width;
+            float fillWidth = _progressFill.resolvedStyle.width;
+            float handleWidth = _handle.resolvedStyle.width;
 
             if (fillWidth > 0 && handleWidth > 0) {
                 // 3. handle의 중심이 fill의 끝에 오도록 left 설정
                 float handleLeft = fillWidth - (handleWidth * 0.5f);
-                handle.style.left = handleLeft;
+                _handle.style.left = handleLeft;
             }
             else {
                 // fallback (렌더링 전이거나 layout 미정일 경우)
-                handle.style.left = Length.Percent(percent * 100f);
+                _handle.style.left = Length.Percent(percent * 100f);
             }
         }
     }
--- Graffiti_Crew/Assets/01.Script/UI/Models/FightModel.cs
+++ Graffiti_Crew/Assets/01.Script/UI/Models/FightModel.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 namespace AH.UI.Models {
     public class FightModel : Model {
         [SerializeField] private LoadStageSO _loadStageSO;
+        [Space]
         [SerializeField] private SliderValueSO _sprayGauage;
         [SerializeField] private SliderValueSO _gameProgressGauage;
-
+        [Space]
+        [SerializeField] private Sprite[] _progressSprites;
+        public LoadStageSO GetLoadStageSO() {
+            return _loadStageSO;
+        }
         public string GetStageName() {
             return _loadStageSO.GetCurrentStageName();
         }
@@ -18,6 +23,9 @@ namespace AH.UI.Models {
             return _sprayGauage;
         }
 
+        public Sprite[] GetProgressSprites() {
+            return _progressSprites;
+        }
         public SliderValueSO GetGameProgressSO() {
             return _gameProgressGauage;
         }
